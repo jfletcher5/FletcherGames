ef8164c288137904206eae1eacf563859a8dfb1b
jfletcher5 <jon.d.fletch@gmail.com>
2024-03-18 14:24:37 -0400

    updates

--- app/src/main/java/com/example/mathgame/GameSettingsActivity.java
+++ app/src/main/java/com/example/mathgame/GameSettingsActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 
 public class GameSettingsActivity extends AppCompatActivity {
 
-    private int timerValue;
+    private int timerValue = 30;
     private TextView timerTextView;
     private Button[] timerButtons = new Button[3];
     private int[] timerValues = {30, 15, 5};
@@ -24,8 +24,16 @@ public class GameSettingsActivity extends AppCompatActivity {
         timerButtons[1] = findViewById(R.id.timerInput2);
         timerButtons[2] = findViewById(R.id.timerInput3);
 
+
+
         findViewById(R.id.submitButton).setOnClickListener(v -> {
             Intent resultIntent = new Intent();
+            //if bounds is not set, set it to 10
+            if (getIntent().getIntExtra("bounds", 0) == 0) {
+                ((EditText) findViewById(R.id.boundsInput)).setText("10");
+            } else {
+                ((EditText) findViewById(R.id.boundsInput)).setText(String.valueOf(getIntent().getIntExtra("bounds", 0)));
+            }
             resultIntent.putExtra("timer", timerValue);
             resultIntent.putExtra("bounds", Integer.parseInt(((EditText) findViewById(R.id.boundsInput)).getText().toString()));
             setResult(RESULT_OK, resultIntent);
